925dbedee369868835fc5e6ff361afb17d76f4de
pganchozo <45433428+pganchozo@users.noreply.github.com>
2019-04-16 15:20:14 -0400

    Add files via upload

---
+++ clkDivider1.v
@@ -0,0 +1,53 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    14:57:08 04/16/2019 
+// Design Name: 
+// Module Name:    clkDivider1 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+
+//clk divider 1 kHz
+module clkDivider1(
+	input clk_in,
+	input rst,
+	output reg divided_clk
+    );
+	 
+	 
+
+parameter toggle_value = 10'b1111101000;;
+	 
+reg[9:0] cnt;
+
+always@(posedge clk_in or posedge rst)
+begin
+	if (rst==1) begin
+		cnt <= 0;
+		divided_clk <= 0;
+	end
+	else begin
+		if (cnt==toggle_value) begin
+			cnt <= 0;
+			divided_clk <= ~divided_clk;
+		end
+		else begin
+			cnt <= cnt +1;
+			divided_clk <= divided_clk;		
+		end
+	end
+
+end
+			  
---
+++ clkDivider2.v
@@ -0,0 +1,51 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    14:58:37 04/16/2019 
+// Design Name: 
+// Module Name:    clkDivider2 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module clkDivider2(
+	input clk_in,
+	input rst,
+	output reg divided_clk
+    );
+	 
+	 
+
+parameter toggle_value = 21'b111111111111111111111;
+	 
+reg[20:0] cnt;
+
+always@(posedge clk_in or posedge rst)
+begin
+	if (rst==1) begin
+		cnt <= 0;
+		divided_clk <= 0;
+	end
+	else begin
+		if (cnt==toggle_value) begin
+			cnt <= 0;
+			divided_clk <= ~divided_clk;
+		end
+		else begin
+			cnt <= cnt +1;
+			divided_clk <= divided_clk;		
+		end
+	end
+
+end
+			  
