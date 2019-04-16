d752ed7db9bd59baada05020d3c17ecc8d233d47
pganchozo <45433428+pganchozo@users.noreply.github.com>
2019-04-16 15:21:36 -0400

    Add files via upload

--- Top.v
+++ Top.v
@@ -27,8 +27,18 @@ output reg [5:0] led;
 output [3:0] AN;
 output [6:0] seven_out;
 
+wire clkOut1, clkOut2;
+wire clear, enter, changeWire;
+
+clkDivider1 clk1(clk_in, rst, clkOut1);
+clkDivider2 clk2(clk_in, rst, clkOut2);
+
+debouncer clearr(clkOut2, rst, clr, clear);
+debouncer enterr(clkOut2, rst, ent, enter);
+debouncer changee(clkOut2, rst, change, changeWire);
 
 
-//ASM asm( 
+//implementing ASM
+ASM asm(.ssd_clk(clkOut1), .clk(clkOut2), .rst(rst), .clr(clear), .ent(enter), .change(changeWire), led, ssd, sw, AN, seven_out);
 
 endmodule
